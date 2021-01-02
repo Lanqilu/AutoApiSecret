697b08330aeed4cbceeee1f0d1e76d054e101626
泷 <46443012+Lanqilu@users.noreply.github.com>
2021-01-03 00:06:34 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,11 @@
 # -*- coding: UTF-8 -*-
+import os
 import requests as req
-import json,sys,time
+import json
+from pathlib import Path
+import time
+import random
+import sys
 #先注册azure应用,确保应用有以下权限:
 #files:	Files.Read.All、Files.ReadWrite.All、Sites.Read.All、Sites.ReadWrite.All
 #user:	User.Read.All、User.ReadWrite.All、Directory.Read.All、Directory.ReadWrite.All
@@ -8,7 +13,18 @@ import json,sys,time
 #注册后一定要再点代表xxx授予管理员同意,否则outlook api无法调用
 
 
-
+api_list = [
+    r'https://graph.microsoft.com/v1.0/me/drive/root',
+    r'https://graph.microsoft.com/v1.0/me/drive',
+    r'https://graph.microsoft.com/v1.0/drive/root',
+    r'https://graph.microsoft.com/v1.0/users',
+    r'https://graph.microsoft.com/v1.0/me/messages',
+    r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',
+    r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',
+    r'https://graph.microsoft.com/v1.0/me/drive/root/children',
+    r'https://graph.microsoft.com/v1.0/me/mailFolders',
+    r'https://graph.microsoft.com/v1.0/me/outlook/masterCategories'
+]
 
 
 
@@ -82,3 +98,36 @@ def main():
         pass
 for _ in range(3):
     main()
+
+def main_invoke():
+    file = filepath.open(mode='r', encoding='utf-8')
+    old_refresh_token = file.read()
+    file.close()
+
+    access_token = get_token(old_refresh_token)
+    headers = {
+        'Authorization': access_token,
+        'Content-Type': 'application/json'
+    }
+
+    random.shuffle(api_list)  # 执行洗牌算法，使得每次相邻调用的顺序不一致
+
+    try:
+        for api_url in api_list:
+            time.sleep(random.randrange(2, 12))
+            if req.get(api_url, headers=headers).status_code == 200:
+                print("调用成功: ", api_url)
+            else:
+                print("调用异常: ", api_url)
+
+        print('此次运行结束时间为: ', time.asctime(time.localtime(time.time())))
+    except:
+        print("调用出现异常，pass")
+        pass
+
+
+for _ in range(random.randrange(3, 6)):
+    time.sleep(60 * random.randrange(1, 8))
+    main_invoke()
+    
+    
